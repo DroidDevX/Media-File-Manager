e2e67ba4d14618d96c77e992be499693a9cd84c2
DroidDevX <ghostmally@gmail.com>
2021-02-07 18:57:39 +0200

    Implemented onClick() event listener in FolderActivity.java
    Whenever the a file item is clicked on the recyclerview, FolderActivity.java
    is made aware and reloads the UI with the contents of the clicked file.

--- app/src/main/java/com/example/filemanager/Activities/FolderActivity.java
+++ app/src/main/java/com/example/filemanager/Activities/FolderActivity.java
@@ -17,7 +17,7 @@ import com.example.filemanager.ViewModel.InternalStorageViewModel;
 import java.io.File;
 import java.util.List;
 
-public class FolderActivity extends AppCompatActivity {
+public class FolderActivity extends AppCompatActivity implements InternalStorageAdapter.OnClickListener {
     private static final String TAG = "InternalStorageFolderDe";
     final public static String ACTION_DISPLAY_DIRECTORY_CONTENTS ="dispDirContents";
 
@@ -40,6 +40,7 @@ public class FolderActivity extends AppCompatActivity {
     public void setupViewmodel(String parentDirectoryPath){
         viewModel = new InternalStorageViewModel(InternalStorageRepository.getInstance(),parentDirectoryPath);
         fileAdapter = new InternalStorageAdapter();
+        fileAdapter.setOnClickListener(this);
         viewModel.getFileListLiveData().observe(this, new Observer<List<File>>() {
             @Override
             public void onChanged(List<File> files) {
@@ -64,4 +65,8 @@ public class FolderActivity extends AppCompatActivity {
         fileRecyclerview.setLayoutManager(new GridLayoutManager(this,4));
     }
 
+    @Override
+    public void onFileClick(File f) {
+        viewModel.getFiles(f.getAbsolutePath());
+    }
 }
